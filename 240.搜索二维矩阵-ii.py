e7a91ae81f5748310f7db7fe3507e48de6f17fed
AutuanLiu <autuanliu@163.com>
2019-08-19 23:05:10 +0800

    add

--- 240.搜索二维矩阵-ii.py
+++ 240.搜索二维矩阵-ii.py
@@ -14,16 +14,16 @@
 # Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n5'
 #
 # 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target。该矩阵具有以下特性：
-# 
-# 
+#
+#
 # 每行的元素从左到右升序排列。
 # 每列的元素从上到下升序排列。
-# 
-# 
+#
+#
 # 示例:
-# 
+#
 # 现有矩阵 matrix 如下：
-# 
+#
 # [
 # ⁠ [1,   4,  7, 11, 15],
 # ⁠ [2,   5,  8, 12, 19],
@@ -31,14 +31,33 @@
 # ⁠ [10, 13, 14, 17, 24],
 # ⁠ [18, 21, 23, 26, 30]
 # ]
-# 
-# 
-# 给定 target = 5，返回 true。
-# 
-# 给定 target = 20，返回 false。
-# 
 #
+#
+# 给定 target = 5，返回 true。
+#
+# 给定 target = 20，返回 false。
+#
+#
+# 每行的元素从左到右升序排列。
+# 每列的元素从上到下升序排列。
+# 那么从左到右从上到下构成一个有序数组，每行的最后一个元素x相当于一个分界点
+# 如果要查找的元素大于x，那么要查找的数字肯定在下一行，如果小于x，那么要查找的元素就在前面那些列
+# 如果相等的话 就返回true，如果搜索完所有的元素，那么就返回false
 class Solution:
     def searchMatrix(self, matrix, target):
-        
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        row, col = 0, n - 1
+        while row < m and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
+        return False
 
+# s = Solution()
+# a = [[1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30]]
+# print(s.searchMatrix(a, 20))
--- 74.搜索二维矩阵.py
+++ 74.搜索二维矩阵.py
@@ -14,14 +14,14 @@
 # Testcase Example:  '[[1,3,5,7],[10,11,16,20],[23,30,34,50]]\n3'
 #
 # 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
-# 
-# 
+#
+#
 # 每行中的整数从左到右按升序排列。
 # 每行的第一个整数大于前一行的最后一个整数。
-# 
-# 
+#
+#
 # 示例 1:
-# 
+#
 # 输入:
 # matrix = [
 # ⁠ [1,   3,  5,  7],
@@ -30,10 +30,10 @@
 # ]
 # target = 3
 # 输出: true
-# 
-# 
+#
+#
 # 示例 2:
-# 
+#
 # 输入:
 # matrix = [
 # ⁠ [1,   3,  5,  7],
@@ -42,9 +42,24 @@
 # ]
 # target = 13
 # 输出: false
-# 
 #
+#
+# 每行中的整数从左到右按升序排列。
+# 每行的第一个整数大于前一行的最后一个整数。
+# z 形有序数组
+# 每行的最后一个元素为 x, 判断target和x的关系，如果小于x，那么target在左边的列
+# 如果大于x，那么target在下一行，否则找到了 target
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        row, col = 0, n - 1
+        while row < m and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                col -= 1
+            else:
+                row += 1
+        return False
