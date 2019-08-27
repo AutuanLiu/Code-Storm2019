70558c80c6e3a96b6139b196026d2ab65c9b880a
AutuanLiu <autuanliu@163.com>
2019-08-27 10:49:28 +0800

    update sort

--- CppScripts/sort/sort/sort.h
+++ CppScripts/sort/sort/sort.h
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <iostream>
 #include <limits>
+#include <stack>
 #include <vector>
 
 namespace sort {
@@ -21,6 +22,9 @@ public:
     void select_sort(std::vector<int>& nums); // 选择排序
     void quick_sort_swap(std::vector<int>& nums, int low, int high); // 快速排序-交换元素
     void quick_sort(std::vector<int>& nums, int low, int high); // 快速排序
+    int get_partition(std::vector<int>& nums, int low, int high); // 快速排序获得分割点
+    int get_partition_swap(std::vector<int>& nums, int low, int high); // 快速排序获得分割点-交换元素
+    void quick_sort_iteration(std::vector<int>& nums, int low, int high); // 快速排序迭代版
     void merge_sorted(std::vector<int>& nums, int left, int mid, int right); // 合并两个有序数组
     void merge_sorted_easy(std::vector<int>& nums, int left, int mid, int right); // 化简版合并两个有序数组
     void merge_sort(std::vector<int>& nums, int left, int right); // 归并排序
--- CppScripts/sort/sort/sort_test.cpp
+++ CppScripts/sort/sort/sort_test.cpp
@@ -10,9 +10,10 @@ int main(void)
     sort::print_vector(nums);
     sort::Sorter sorter;
     //sorter.merge_sort(nums, 0, n - 1); // 归并排序
-    sorter.merge_sort_iteration(nums, 0, n - 1); // 归并排序迭代版
+    //sorter.merge_sort_iteration(nums, 0, n - 1); // 归并排序迭代版
     //sorter.quick_sort(nums, 0, n - 1); // 快速排序
     //sorter.quick_sort_swap(nums, 0, n - 1); // 快速排序
+    sorter.quick_sort_iteration(nums, 0, n - 1); // 快速排序迭代版
     //sorter.heap_sort(nums); // 堆排序
     //sorter.shell_sort(nums); // 希尔排序
     //sorter.select_sort(nums); // 选择排序
