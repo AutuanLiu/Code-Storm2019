3876bcd20ab94b569d36287e7d4c20a042066acc
AutuanLiu <autuanliu@163.com>
2019-09-26 14:49:36 +0800

    add Scala support

---
+++ Scala/trait_extends.scala
@@ -0,0 +1,20 @@
+trait Greeter {
+  def greet(name: String): Unit =
+    println("Hello, " + name + "!")
+} // 默认实现
+
+// 默认类继承
+class DefaultGreeter extends Greeter
+
+// 自定义继承
+class CustomizalableGreeter(prefix: String, postfix: String) extends Greeter {
+	override def greet(name: String): Unit = {
+  	println(prefix + name + postfix)
+  }
+}
+
+val greeter = new DefaultGreeter()
+greeter.greet("Scala") // Hello, Scala!
+
+val greeter1 = new CustomizalableGreeter("Hello, ", "!")
+greeter1.greet("Scala") // Hello, Scala!
